1cc3ba0de3d4b9cf79ea0e7dba30498cc89f026f
Icaro Gabriel dos Santos Moreira <icaro.moreira@protonmail.com>
2021-02-17 23:25:46 -0300

    Delete Falencia cardiaca directory

--- Falencia cardiaca/HeartFailure-NB.py
+++
@@ -1,9 +0,0 @@
-import pandas as PD
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.preprocessing import LabelEncoder
-
-dataset = PD.read_csv("heart_failure_clinical_records_dataset.csv")
-
-trainDataframe, validationDataframe = train_test_split(dataframe, test_size = 0.25, random_state = 1)
